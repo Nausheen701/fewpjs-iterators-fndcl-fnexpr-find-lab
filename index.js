95a32aaf6adb6a0852d00f3d99cc2d40fed23ea7
Nausheen Akhter <nausheen701@gmail.com>
2021-11-28 14:27:41 +0300

    commit tree 1530fd9b10aaec738b1f114829f110db13115699

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+// const record = [
+//     {year: "2018", result: "L"},
+//     {year: "2017", result: "W"},
+//     {year: "2016", result: "N/A"}
+//     //...
+//   ]
+  
+superbowlWin = (record) => {
+    let result = record.find( record => record.result === "W" )
+    return !!result ? result.year : undefined
+  }
 
+// const superbowlWin = record.find(function (record) {
+// function superbowlWin(record) {
+//     if (record.result === 'W') {
+//         return Object.assign({}, record, {
+//             year: record.year,
+//     })
+// } else {
+//         return "undefined"
+//     }
+// }
